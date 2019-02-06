cff8239d1b923317d0d9bf65a56b48f11495ad98
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2019-02-05 23:36:06 -0500

    validacion

--- src/app/page/index/index.component.ts
+++ src/app/page/index/index.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ɵConsole } from '@angular/core';
-import { AlternanciaService } from '../../service/alternancia.service';
-import { AsistenciaService } from '../../service/asistencia.service';
+import {Component, OnInit, ɵConsole} from '@angular/core';
+import {AlternanciaService} from '../../service/alternancia.service';
+import {AsistenciaService} from '../../service/asistencia.service';
 
 
 @Component({
@@ -12,7 +12,7 @@ export class IndexComponent implements OnInit {
 
   alternancia: any = [];
 
-  constructor(private alternanciaService: AlternanciaService,private asistenciaService: AsistenciaService) {
+  constructor(private alternanciaService: AlternanciaService, private asistenciaService: AsistenciaService) {
     this.getcodaltdet();
   }
 
@@ -21,11 +21,15 @@ export class IndexComponent implements OnInit {
 
   getcodaltdet() {
     this.alternanciaService.traerCodigoAltdet().subscribe(data => {
-      this.alternancia =  data;
-      console.log(this.alternancia);
-      this.asistenciaService.asistencia(this.alternancia.codaltdet).subscribe(asistencia => console.log(asistencia));
-    }
-      );
+        this.alternancia = JSON.parse(JSON.stringify(data));
+        console.log(this.alternancia);
+
+        this.asistenciaService.asistencia(this.alternancia.codaltdet).subscribe(asistencia => {
+          console.log(asistencia);
+        });
+
+      }
+    );
   }
 
 }
